80bd0544bce54be19cc7f6d93f086128bd0218eb
Jeff Hudzik <jrhudzik@yahoo.com>
2014-11-21 19:50:51 -0500

    Search Form Updates

--- app/scripts/search/controllers/search-form.js
+++ app/scripts/search/controllers/search-form.js
@@ -3,25 +3,37 @@
 
 angular.module('pfSearch')
 	.controller('searchFormCtrl',
-		function($scope, $location, searchFactory) {
+		function($scope, $rootScope, $location, searchFactory, $interval) {
+			$rootScope.searchTerms = [];
+			$scope.collapsed = true;
+			$scope.datesCollapsed = true;
 			$scope.searchParams = {
-				cap: 50,
+				cap: 10,
 				size: 'z'
 			};
 			/**
-			 * 
+			 *
 			 */
 			 $scope.search = function() {
 			 	searchFactory
 			 		.search($scope.searchParams)
 			 		.then(
-			 			function(resp) {
+			 			function() {
+			 				$rootScope.searchTerms.unshift($scope.searchParams.tags);
 			 				$location.url('/results');
 			 			},
 			 			function(err) {
-			 				
 			 			}
 			 		)
+			 		.finally(function() {
+			 			$scope.collapsed = true;
+			 		});
 			 };
+			 $scope.toggleDates = function() {
+			 	$scope.datesCollapsed = !$scope.datesCollapsed;
+			 };
+			 $scope.$on('menuToggle', function() {
+			 	$scope.collapsed = !$scope.collapsed;
+			 });
 		}
 	);
